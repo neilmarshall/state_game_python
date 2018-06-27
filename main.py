5479c0bb3ace0e7d4ac7900700ae951eb9a2b739
Neil <neil.marshall@dunelm.org.uk>
2018-06-27 08:50:38 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+The aim of this program is to require users to name a US state that
+begins with a specified letter.
+"""
+
+from game import Game
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
