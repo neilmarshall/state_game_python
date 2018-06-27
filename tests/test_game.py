4b5a3ff5519fcdbb96199d75f07ee1f9f902ab3c
Neil <neil.marshall@dunelm.org.uk>
2018-06-27 14:44:10 +0100

    Update test_game.py

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,14 @@
 import unittest
+import unittest.mock as mock
+
 from game import Game
 
-class TestGame(unittest.TestCase):
+class TestGameWithMockData(unittest.TestCase):
+
+    @mock.patch.object(Game, "load_data")
     def setUp(self):
+        patch.return_value = ['Iowa', 'Ohio', 'Utah']
         self.game = Game()
+        
+    def test_class_instantiation(self):
+        self.assertListEqual(self.game.state_list, ['Iowa', 'Ohio', 'Utah'])
