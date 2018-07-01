0d70a10bc3042e43011edd9c2773481f33976e40
neilmarshall <neil.marshall@dunelm.org.uk>
2018-07-01 10:25:31 +0100

    Add test cases for 'play_round' method with correct first guess

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,11 +3,12 @@ import unittest.mock as mock
 
 from game import Game
 
+
 class TestGameWithMockData(unittest.TestCase):
 
     @mock.patch.object(Game, "load_data")
-    def setUp(self, game_patch):
-        game_patch.return_value = ['Iowa', 'Ohio', 'Utah']
+    def setUp(self, load_data_patch):
+        load_data_patch.return_value = ['Iowa', 'Ohio', 'Utah']
         self.game = Game(None)
         
     def test_class_instantiation(self):
@@ -33,3 +34,24 @@ class TestGameWithMockData(unittest.TestCase):
         self.assertEqual(3, print_patch.call_count)
         self.assertEqual(2, input_patch.call_count)
         print_patch.assert_called_with("Game Exited!!!")
+
+    @mock.patch('builtins.input')
+    @mock.patch('builtins.print')
+    def test_class_play_round_method_correct_guess_with_upper_case(self, print_patch, input_patch):
+        input_patch.return_value = 'UTAH'
+        self.game.play_round()
+        print_patch.assert_called_with("Congratulations - you guessed 'Utah' correctly!!!")
+
+    @mock.patch('builtins.input')
+    @mock.patch('builtins.print')
+    def test_class_play_round_method_correct_guess_with_lower_case(self, print_patch, input_patch):
+        input_patch.return_value = 'utah'
+        self.game.play_round()
+        print_patch.assert_called_with("Congratulations - you guessed 'Utah' correctly!!!")
+
+    @mock.patch('builtins.input')
+    @mock.patch('builtins.print')
+    def test_class_play_round_method_correct_guess_with_mismatched_case(self, print_patch, input_patch):
+        input_patch.return_value = 'uTaH'
+        self.game.play_round()
+        print_patch.assert_called_with("Congratulations - you guessed 'Utah' correctly!!!")
